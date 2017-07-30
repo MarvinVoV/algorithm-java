1fcfba4cd04834a4089b116b265c8cacf6ee38d0
MarvinVoV <marvin_vov@outlook.com>
2017-07-31 01:56:51 +0800

    fix

--- src/main/java/sun/algorithm/leetcode/LongestCommonSubSequence.java
+++ src/main/java/sun/algorithm/leetcode/LongestCommonSubSequence.java
@@ -1,5 +1,6 @@
 package sun.algorithm.leetcode;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -114,77 +115,71 @@ public class LongestCommonSubSequence {
     }
 
     /**
-     * 递归求解
+     * 递归求解最长公共子序列的长度; 二维数组中，长度最大的为最优解的长度;
+     * 在递归求解中，会重复求解子问题
+     * <pre>
      *              0                           if i == 0 or j == 0
      * c[i,j] =     c[i-1, j-1] + 1             if i,j > 0 and x[i] = y[j]
      *              max(c[i,j-1], c[i-1,j])     if i,j > 0 and x[i] != y[i]
+     * </pre>
      *
-     * @param x
-     * @param y
+     * @param x 序列X
+     * @param y 序列Y
      * @param c c[i,j]表示Xi和Yi的lcs的长度
-     * @param i
-     * @param j
+     * @param i 序列X的长度
+     * @param j 序列Y的长度
      */
-    // todo Error
-    private void recursionSolution(char[] x, char[] y, int[][] c, int i, int j) {
+    private void recursionSolutionLcs(char[] x, char[] y, int[][] c, int i, int j) {
         if (i == 0 || j == 0) {
             c[i][j] = 0;
             return;
         }
         if (x[i - 1] == y[j - 1]) {
-            recursionSolution(x, y, c, i - 1, j - 1);
+            recursionSolutionLcs(x, y, c, i - 1, j - 1);
             c[i][j] = c[i - 1][j - 1] + 1;
-            System.out.println("---" + c[i][j] + "  i=" + i + "  j=" + j);
         } else {
-            recursionSolution(x, y, c, i - 1, j);
-            recursionSolution(x, y, c, i, j - 1);
-            c[i][j] = Math.max(c[i][j - 1], c[i - 1][j]);
-//            System.out.println(c[i][j-1] + " " + c[i-1][j] + "  " + c[i][j]);
+            recursionSolutionLcs(x, y, c, i - 1, j);
+            recursionSolutionLcs(x, y, c, i, j - 1);
+            c[i][j] = Math.max(c[i - 1][j], c[i][j - 1]);
         }
     }
 
+    private static String x;
+    private static String y;
+
+    @Before
+    public void init() {
+        x = "ABCBDAB";
+        y = "BDCABA";
+
+    }
 
     @Test
     public void testLcs() {
-        String x = "ABCBDAB";
-        String y = "BDCABA";
-        LongestCommonSubSequence lcs = new LongestCommonSubSequence();
-
-        Data data = lcs.lcsLength(x.toCharArray(), y.toCharArray());
-        // print table b
-//        for (int i = 0; i < data.b.length; i++) {
-//            for (int j = 0; j < data.b[i].length; j++) {
-//                System.out.print(data.b[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        Data data = lcsLength(x.toCharArray(), y.toCharArray());
         // print table c
-        for (int i = 0; i < data.c.length; i++) {
-            for (int j = 0; j < data.c[i].length; j++) {
-                System.out.print(data.c[i][j] + " ");
-            }
-            System.out.println();
-        }
+        printOptimalSolutionArray(data.c);
+
         printLcs(data.b, x.toCharArray(), x.length(), y.length()); // BCBA
         System.out.println();
         printLcs2(data.c, x.toCharArray(), x.length(), y.length()); // BCBA
+        System.out.println();
     }
 
     @Test
-    public void testRecursionSolution() {
-        String x = "ABCBDAB";
-        String y = "BDCABA";
-        LongestCommonSubSequence lcs = new LongestCommonSubSequence();
+    public void testRecursionSolutionLcs() {
         int[][] c = new int[x.length() + 1][y.length() + 1];
-        lcs.recursionSolution(x.toCharArray(), y.toCharArray(), c, x.length(), y.length());
+        recursionSolutionLcs(x.toCharArray(), y.toCharArray(), c, x.length(), y.length());
+        printOptimalSolutionArray(c);
+    }
 
-        // print table c
+
+    private void printOptimalSolutionArray(int[][] c) {
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < c[i].length; j++) {
                 System.out.print(c[i][j] + " ");
             }
             System.out.println();
         }
-//        printLcs2(c, x.toCharArray(), x.length(), y.length());
     }
 }
