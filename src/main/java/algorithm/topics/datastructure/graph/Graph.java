ce6d19fd4f0adb6794e2b022bbe57264e1f52027
MarvinVoV <marvin_vov@outlook.com>
2022-03-21 22:50:30 +0800

    add graph bst

---
+++ src/main/java/algorithm/topics/datastructure/graph/Graph.java
@@ -0,0 +1,99 @@
+package algorithm.topics.datastructure.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 无向图
+ */
+public class Graph {
+    // 顶点个数
+    private int v;
+
+    // 邻接表
+    private LinkedList<Integer>[] adj;
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    /**
+     * 无向图的一条边存储两次
+     *
+     * @param s 起始点
+     * @param t 终止点
+     */
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    /**
+     * 广度优先搜索(Breadth First Search)
+     *
+     * @param s
+     * @param t
+     */
+    public void bsf(int s, int t) {
+        if (s == t) {
+            return;
+        }
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        int[] prev = new int[v];
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); i++) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    /**
+     * 递归输出s到t的路径
+     *
+     * @param prev
+     * @param s
+     * @param t
+     */
+    private void print(int[] prev, int s, int t) {
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.println(t + " ");
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(8);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(6, 7);
+
+        graph.bsf(0, 6);
+    }
+}
