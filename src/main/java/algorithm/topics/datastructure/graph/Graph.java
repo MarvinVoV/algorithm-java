ad0350254d86a1cfaad6c5f26632a55ec4bb96b2
MarvinVoV <marvin_vov@outlook.com>
2022-03-26 15:52:32 +0800

    topo sort

--- src/main/java/algorithm/topics/datastructure/graph/Graph.java
+++ src/main/java/algorithm/topics/datastructure/graph/Graph.java
@@ -13,6 +13,9 @@ public class Graph {
     // 邻接表
     private LinkedList<Integer>[] adj;
 
+    // 递归遍历方法中使用
+    private boolean found = false;
+
     public Graph(int v) {
         this.v = v;
         adj = new LinkedList[v];
@@ -29,7 +32,7 @@ public class Graph {
      */
     public void addEdge(int s, int t) {
         adj[s].add(t);
-        adj[t].add(s);
+//        adj[t].add(s);
     }
 
     /**
@@ -68,6 +71,40 @@ public class Graph {
     }
 
     /**
+     * 深度优先搜素 Depth First Search (DFS)
+     *
+     * @param s
+     * @param t
+     */
+    public void dfs(int s, int t) {
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, prev, visited);
+        print(prev, s, t);
+    }
+
+    private void recurDfs(int w, int t, int[] prev, boolean[] visited) {
+        if (found) {
+            return;
+        }
+        visited[w] = true;
+        if (w == t) {
+            found = true;
+            return;
+        }
+        for (int i = 0; i < adj[w].size(); i++) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, prev, visited);
+            }
+        }
+    }
+
+    /**
      * 递归输出s到t的路径
      *
      * @param prev
@@ -75,10 +112,88 @@ public class Graph {
      * @param t
      */
     private void print(int[] prev, int s, int t) {
+        StringBuilder sb = new StringBuilder();
+        doPrint(prev, s, t, sb);
+        System.out.println(sb);
+    }
+
+    private void doPrint(int[] prev, int s, int t, StringBuilder sb) {
         if (prev[t] != -1 && t != s) {
-            print(prev, s, prev[t]);
+            doPrint(prev, s, prev[t], sb);
         }
-        System.out.println(t + " ");
+        sb.append(t).append(" ");
+    }
+
+
+    /**
+     * Kahn 拓扑排序
+     */
+    public void topoSortByKahn() {
+        // 入度
+        int[] inDegree = new int[v];
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                int w = adj[i].get(j);
+                inDegree[w]++;
+            }
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < v; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int i = queue.remove();
+            System.out.print("->" + i);
+            for (int j = 0; j < adj[i].size(); j++) {
+                int k = adj[i].get(j);
+                inDegree[k]--;
+                if (inDegree[k] == 0) {
+                    queue.add(k);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+
+    /**
+     * 深度优先搜索实现拓扑排序
+     */
+    public void topoSortByDfs() {
+        // 先构建逆向邻接表
+        LinkedList<Integer> inverseAdj[] = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            inverseAdj[i] = new LinkedList<>();
+        }
+        // 通过邻接表生成逆向邻接表
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                int w = adj[i].get(j);
+                inverseAdj[w].add(i);
+            }
+        }
+        boolean[] visited = new boolean[v];
+        // 深度优先遍历
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+            }
+            dfs(i, inverseAdj, visited);
+        }
+    }
+
+    private void dfs(int vertex, LinkedList<Integer> inverseAdj[], boolean[] visited) {
+        for (int i = 0; i < inverseAdj[vertex].size(); i++) {
+            int w = inverseAdj[vertex].get(i);
+            if (visited[w]) {
+                continue;
+            }
+            visited[w] = true;
+            dfs(w, inverseAdj, visited);
+        }
+        System.out.print("->" + vertex);
     }
 
     public static void main(String[] args) {
@@ -95,5 +210,9 @@ public class Graph {
         graph.addEdge(6, 7);
 
         graph.bsf(0, 6);
+        graph.dfs(0, 6);
+
+        graph.topoSortByKahn();
+        graph.topoSortByDfs();
     }
 }
