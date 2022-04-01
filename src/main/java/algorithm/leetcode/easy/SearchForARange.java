300d4668c83ed16dc9bcd5e7b37e36e6a36111c3
MarvinVoV <marvin_vov@outlook.com>
2022-04-01 23:07:08 +0800

    binary search

--- src/main/java/algorithm/leetcode/easy/SearchForARange.java
+++ src/main/java/algorithm/leetcode/easy/SearchForARange.java
@@ -1,5 +1,7 @@
 package algorithm.leetcode.easy;
 
+import java.util.Arrays;
+
 /**
  * Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
  * <p>
@@ -21,10 +23,9 @@ package algorithm.leetcode.easy;
 public class SearchForARange {
 
     /**
-     * Runtime beats 13.19% of java submissions.
-     * todo: recursive -> loop
+     * 范围查找
      *
-     * @param nums
+     * @param nums   nums
      * @param target
      * @return
      */
@@ -37,26 +38,36 @@ public class SearchForARange {
 
     private static int[] binarySearch(int[] nums, int start, int end, int target) {
         if (start == end) {
-            if (nums[start] == target)
+            if (nums[start] == target) {
                 return new int[]{start, end};
-            else
+            } else {
                 return new int[]{-1, -1};
+            }
         }
-        int mid = (start + end) / 2;
+        int mid = start + (end - start) / 2;
 
         int[] left = binarySearch(nums, start, mid, target);
         int[] right = binarySearch(nums, mid + 1, end, target);
 
+        // 左右都没找到
         if (left[1] < 0 && right[1] < 0) {
             return new int[]{-1, -1};
         }
-        if (left[1] < 0 && right[1] >= 0) {
+        // 左边没有找到，右边找到了 返回右边的值
+        if (left[1] < 0) {
             return right;
         }
-        if (left[1] >= 0 && right[1] < 0) {
+        //  右边没有找到，左边找到了，返回左边的值
+        if (right[1] < 0) {
             return left;
         }
         return new int[]{left[0], right[1]};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
+        int[] res = searchRange(nums, 8);
+        System.out.println(Arrays.toString(res));
     }
 
 }
--- src/main/java/algorithm/topics/search/BinarySearch.java
+++ src/main/java/algorithm/topics/search/BinarySearch.java
@@ -4,7 +4,13 @@ package algorithm.topics.search;
  * Created by sunyamorn on 4/12/16.
  */
 public class BinarySearch {
-
+    /**
+     * 非递归方式
+     *
+     * @param a
+     * @param v
+     * @return
+     */
     public static int binarySearch(int[] a, int v) {
         int low = 0;
         int high = a.length - 1;
@@ -21,11 +27,35 @@ public class BinarySearch {
         return -(low + 1); // not found
     }
 
+    /**
+     * 递归方式二分法搜索
+     *
+     * @param a
+     * @param start
+     * @param end
+     * @param v     目标值
+     * @return
+     */
+    public static int binarySearchByRecursive(int[] a, int start, int end, int v) {
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+        int mid = (start + end) >>> 1;
+//        int mid = start + (end -start ) / 2 ;
+        if (a[mid] == v) {
+            return mid;
+        }
+        return v > a[mid] ? binarySearchByRecursive(a, mid + 1, end, v) :
+                binarySearchByRecursive(a, start, mid - 1, v);
+    }
+
+
     public static void main(String[] args) {
         int[] a = {1, 3, 4, 5, 8, 10, 34, 57, 35, 67};
         int v = 4;
         int result = BinarySearch.binarySearch(a, v);
         System.out.println(result);
-        System.out.println(a[result]);
+        result = BinarySearch.binarySearchByRecursive(a, 0, a.length - 1, v);
+        System.out.println(result);
     }
 }
