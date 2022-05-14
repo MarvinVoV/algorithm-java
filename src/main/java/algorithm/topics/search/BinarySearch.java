f24e8c01509ccb0750e72bd7167ef6ca7dbcaea3
MarvinVoV <marvin_vov@outlook.com>
2022-05-14 23:29:10 +0800

    fix

--- src/main/java/algorithm/topics/search/BinarySearch.java
+++ src/main/java/algorithm/topics/search/BinarySearch.java
@@ -5,6 +5,29 @@ package algorithm.topics.search;
  */
 public class BinarySearch {
     /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+    /**
      * 非递归方式
      *
      * @param a
@@ -57,5 +80,8 @@ public class BinarySearch {
         System.out.println(result);
         result = BinarySearch.binarySearchByRecursive(a, 0, a.length - 1, v);
         System.out.println(result);
+
+        int[] nums = new int[]{1, 3, 5, 6};
+        System.out.println(searchInsert(nums, 5));
     }
 }
